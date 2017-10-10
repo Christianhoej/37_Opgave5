0ed4ef18a03981a61a0f7044dcec95fe60d8b165
Adam <32641493+cjjadam@users.noreply.github.com>
2017-10-10 14:12:47 +0200

    Update Dice.java

--- src/Dice.java
+++ src/Dice.java
@@ -1,16 +1,15 @@
 
 public class Dice {
+	privat final int MAX=6;
+	
 	// roll the die and return the value (1-6)
 	public int roll() {
-		float d1=(float)Math.random();     // 0-1
-		float d2=d1*5;                     // 0-5
-		int d3=Math.round(d2);             // 0-5 integer
-		return d3 + 1;                     // 1-6		
+		return (int) (Math.random()*MAX)+1;
 	}
 	
 	// roll the die n times and print the values	
 	public void rollMultiple(int n) {
-		for (int i=0; i<n; i++) { // har rettet fejlen hvor den tager ét kast mindre end indtastet
+		for (int i=0; i<n; i++) { 
 			System.out.print(roll() + " ");
 		}		
 	}
